170f9ce8e52cadcc18e36b06c4d6c5220de39b02
Mosagwe <mospeto@gmail.com>
2020-09-17 09:00:38 +0300

    Updates on mixjs

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,3 +15,9 @@ mix.js('resources/js/vendor.js', 'public/js/vendor.bundle.js')
     .js('resources/js/scripts.js', 'public/js/scripts.bundle.js')
     .sass('resources/scss/vendor.scss', 'public/css/vendor.bundle.css')
     .sass('resources/scss/style.scss', 'public/css/style.bundle.css');
+
+     mix.webpackConfig({
+    output: {
+        publicPath: '/public/',
+    }
+ });
