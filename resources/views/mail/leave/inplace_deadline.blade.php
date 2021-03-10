861f3eed10fb83d8fd9a94374329c1eadc83aa89
Mosagwe <mospeto@gmail.com>
2021-03-10 07:00:55 +0300

    Format Inplace Overdue mail component

--- resources/views/mail/leave/inplace_deadline.blade.php
+++ resources/views/mail/leave/inplace_deadline.blade.php
@@ -1,18 +1,18 @@
 @component('mail::message')
-    # Hello {{ $leaveRequest->employeeInplace->name }},
 
-    This is just a polite reminder that {{ $leaveRequest->applicant->name }} had requested for you to be in place while on leave from
-    {{ $leaveRequest->start_at->format(config('custom.date_format')) }} to {{ $leaveRequest->end_at->format(config('custom.date_format')) }},
-    which was sent on {{ $leaveRequest->created_at->format(config('custom.date_format')) }}.
+# Hello {{ $leaveRequest->employeeInplace->name }},
 
-    Kindly take a moment and log in to accept or decline the request.
+This is just a polite reminder that {{ $leaveRequest->applicant->name }} had requested for you to be in place while on leave from
+{{ $leaveRequest->start_at->format(config('custom.date_format')) }} to {{ $leaveRequest->end_at->format(config('custom.date_format')) }},
+which was sent on {{ $leaveRequest->created_at->format(config('custom.date_format')) }}.
 
-    Thanks in advance for your cooperation.
+Kindly take a moment and log in to accept or decline the request.
 
-    @component('mail::button', ['url' => route('login')])
-        Log In
-    @endcomponent
+Thanks in advance for your cooperation.
+@component('mail::button', ['url' => route('login')])
+    Log In
+@endcomponent
 
-    Thanks,<br>
-    {{ config('app.name') }}
+Thanks,<br>
+{{ config('app.name') }}
 @endcomponent
